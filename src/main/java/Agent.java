cd3bbc729152388a96ff271b90a24cdecea8f7aa
SherlockShellingford <48645970+SherlockShellingford@users.noreply.github.com>
2020-10-23 12:00:35 -0700

    Wrote generic agent

---
+++ src/main/java/Agent.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+
+public abstract class Agent {
+    State currState;
+    List<Action> seq=new LinkedList<Action>();
+    Goal goal=null;
+    Problem problemFormulation;
+    public abstract Action processNextAction(Preception preception);
+
+    public State getCurrState() {
+        return currState;
+    }
+
+    public void setCurrState(State currState) {
+        this.currState = currState;
+    }
+
+    public List<Action> getSeq() {
+        return seq;
+    }
+
+    public void setSeq(List<Action> seq) {
+        this.seq = seq;
+    }
+
+    public Goal getGoal() {
+        return goal;
+    }
+
+    public void setGoal(Goal goal) {
+        this.goal = goal;
+    }
+
+    public Problem getProblemFormulation() {
+        return problemFormulation;
+    }
+
+    public void setProblemFormulation(Problem problemFormulation) {
+        this.problemFormulation = problemFormulation;
+    }
+}
